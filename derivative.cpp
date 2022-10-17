8478108d05b490a7287ff550781f15b8a36493c6
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-10-17 11:14:49 +0200

    uwu

--- derivative.cpp
+++ derivative.cpp
@@ -5,13 +5,12 @@
 using namespace std;
 
 const int steps = 1E4;
-double f[steps];
-double fp[steps];
 double fpp[steps];
-
+double fp[steps];
+double f[steps];
 double x_min = 0;
 double x_max = 100;
-const double t =  (x_max - x_min)/ double(steps);
+double t =  (x_max - x_min)/ double(steps);
 
 void function_def(int steps_f){
     for (int i = 0; i < steps_f; i++){
@@ -19,24 +18,28 @@ void function_def(int steps_f){
     }
 }
 
-void derivative(){
-    fp[0] = (f[1]- f[0]) / t;
+void derivative(double func[], double t){
+    double fp[sizeof(f)];
+
+    fp[0] = (func[1]- func[0]) / t;
     for (int i = 1; i < (steps - 1); i++){
         fp[i] = (f[i+1] - f[i-1]) / (2*t);
     }
     fp[int(steps)-1] = (f[int(steps)-1]- f[int(steps) - 2]) / t;
 }
 
-void second_derivative(){
+double * second_derivative(double func[], double t){
+    // double t = (x_max - x_min) / double(steps);
     for (int i = 1; i < (steps - 1); i++){
-        fpp[i] = (f[i+1] - 2*f[i] + f[i-1]) / (pow(t, 2));
+        fpp[i] = (func[i+1] - 2*func[i] + func[i-1]) / (pow(t, 2));
     }
+    return fpp;
 }
 int main(){
 
     function_def(steps);
-    derivative();
-    second_derivative();
+    derivative(f, t);
+    second_derivative(f, t);
 
 
     FILE* file;
