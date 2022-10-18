5ff6b22fb447bf38d87c566d6606a4dae4b9ced7
Carlos Herrera Vázquez <84268470+Lentejas1@users.noreply.github.com>
2022-10-18 23:43:27 +0200

    Working!

--- derivative.cpp
+++ derivative.cpp
@@ -2,53 +2,54 @@
 # include <cmath>
 # include <cstdio>
 
-using namespace std;
-
-const int steps = 1E4;
-double fpp[steps];
-double fp[steps];
-double f[steps];
-double x_min = 0;
-double x_max = 100;
-double t =  (x_max - x_min)/ double(steps);
-
-void function_def(int steps_f){
-    for (int i = 0; i < steps_f; i++){
-        f[i] = pow(float(i)*t, 2) / 2;
+double *function_def(int steps_f, double t){
+    auto *f = new double[steps_f];
+    for (int i = 0; i < steps_f; i++) {
+        f[i] = pow(float(i) * t, 2) / 2;
     }
+    return f;
 }
 
-void derivative(double func[], double t){
-    double fp[sizeof(f)];
-
-    fp[0] = (func[1]- func[0]) / t;
-    for (int i = 1; i < (steps - 1); i++){
-        fp[i] = (f[i+1] - f[i-1]) / (2*t);
+double *derivative(const double *func, int step, double t){
+    auto *fp= new double[step];
+    fp[0] = (func[1] - func[0]) / t;
+    for (int i = 1; i < (step - 1); i++){
+        fp[i] = (func[i+1] - func[i-1]) / (2*t);
     }
-    fp[int(steps)-1] = (f[int(steps)-1]- f[int(steps) - 2]) / t;
+    fp[step-1] = (func[step-1]- func[step - 2]) / t;
+    return fp;
 }
 
-double * second_derivative(double func[], double t){
-    // double t = (x_max - x_min) / double(steps);
-    for (int i = 1; i < (steps - 1); i++){
+double *second_derivative(const double * func, int step, double t){
+    auto *fpp = new double[step];
+    for (int i = 1; i < (step - 1); i++){
         fpp[i] = (func[i+1] - 2*func[i] + func[i-1]) / (pow(t, 2));
     }
     return fpp;
 }
-int main(){
 
-    function_def(steps);
-    derivative(f, t);
-    second_derivative(f, t);
+int main(){
+    int steps = 1E5;
+    std::cout << "Primer check-point\n";
+    double x_min = 0, x_max = 100;
+    double t = (x_max - x_min) / double(steps);
+
+    double *f = function_def(steps, t);
+    double *fp = derivative(f, steps, t);
+    std::cout << "Segundo check-point\n";
+    double *fpp = second_derivative(f, steps, t);
+    std::cout << "Tercer check-point, f''(" << steps << ")=" << fpp[steps-1];
+
 
 
     FILE* file;
     file = fopen("derivatives.dat", "w" );
-    for (int i = 0; i < 100; i = i+1){
+    for (int i = 0; i < 100; i++){
 
-        fprintf(file, "%f \t %f \t %f \t %f \n", i*t, f[i] , fp[i] , fpp[i]);
+        fprintf(file, "%f \t %f \t %f \n", f[i] , fp[i] , fpp[i]);
 
     }
 
-    fclose(file); //Cerramos el fichero
+    fclose(file); //Closes the file
+    return 0;
 }
